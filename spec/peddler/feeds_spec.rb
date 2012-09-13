96c6b78cf6cd5c2b43d428a456fce11a9d2d84f4
Gilles Deleuze <gilles.deleuze@papercavalier.com>
2012-09-13 10:39:18 +0000

    Implement the Feeds service

--- spec/peddler/feeds_spec.rb
+++ spec/peddler/feeds_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../spec_helper'
 
 module Peddler
-  describe Products do
-    let(:service) { Products.new 'US' }
+  describe Feeds do
+    let(:service) { Feeds.new 'US' }
 
     it 'gets the service status' do
       service.configure key:    'foo',
