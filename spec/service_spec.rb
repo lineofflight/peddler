d68521424543d58d3802f1f676c1dd8415967182
hakanensari <hakan.ensari@papercavalier.com>
2013-08-05 03:20:06 +0100

    Rewrite Service

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -4,7 +4,7 @@ module Peddler
   describe Service do
     before do
       @klass = Class.new(Service)
-      @service = @klass.new('US')
+      @service = @klass.new('US', 'key', 'secret', 'seller')
     end
 
     it 'configures the endpoint path' do
@@ -12,15 +12,12 @@ module Peddler
       @service.endpoint.must_match(/Foo$/)
     end
 
-    it 'has a User-Agent header' do
+    it 'has a User Agent' do
       @service.connection.data[:headers].wont_be_empty
     end
 
     it 'gets the service status' do
-      @service.configure(aws_access_key_id:    'foo',
-                         aws_secret_access_key: 'bar',
-                         seller: 'baz')
-      @service.status.must_match(/GREEN|YELLOW|RED/)
+      @service.get_service_status.must_match(/GREEN|YELLOW|RED/)
     end
   end
 end
