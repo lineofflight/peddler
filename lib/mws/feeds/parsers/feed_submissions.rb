51081c503d278f9b5ec75c2924963118a14c0551
hakanensari <hakan.ensari@papercavalier.com>
2013-10-02 16:59:52 +0100

    Add Feed Submissions Parser

---
+++ lib/mws/feeds/parsers/feed_submissions.rb
@@ -0,0 +1,20 @@
+require 'peddler/parsers/collection'
+require 'mws/feeds/parsers/feed_submission'
+
+module MWS
+  module Feeds
+    module Parsers
+      class FeedSubmissions < ::Peddler::Parsers::Collection
+        def each(&blk)
+          feed_submission_nodes.each { |node| yield FeedSubmission.new(node) }
+        end
+
+        private
+
+        def feed_submission_nodes
+          xpath('FeedSubmissionInfo')
+        end
+      end
+    end
+  end
+end
