7be9b37b294400e1f631eebc4ef427635c26516b
hakanensari <hakan.ensari@papercavalier.com>
2014-01-22 18:13:16 +0000

    Allow passing a block to Reports::Requests::Report#get

--- lib/mws/reports/requests/report.rb
+++ lib/mws/reports/requests/report.rb
@@ -5,12 +5,12 @@ module MWS
   module Reports
     module Requests
       class Report < ::Peddler::Request
-        def get(id)
+        def get(id, &blk)
           parameters(:get_report)
             .update(report_id: id)
             .camelize_keys!
 
-          execute
+          execute(&blk)
         end
       end
     end
