ea6067ffdc44abfb82ed76798e6bab85f0adf344
Piotr Misiurek <p.misiurek@gmail.com>
2014-01-23 16:10:51 +0100

    MWS::Products::Parser::OfferListings: fixing test

--- test/mws/products/parsers/test_offer_listings.rb
+++ test/mws/products/parsers/test_offer_listings.rb
@@ -3,13 +3,12 @@ require 'mws/products/parsers/offer_listings'
 
 class OfferListingsParserTest < ParserTest
   def setup
-    node = fixture_from_cassette('offer_listings_test/test_get_lower_for_sku')
+    node = fixture_from_cassette('offer_listings_test/test_get_lower_for_sku').xpath('//xmlns:GetLowestOfferListingsForSKUResult')
     @offer_listings = MWS::Products::Parsers::OfferListings.new(node)
   end
 
   def test_product
     assert_kind_of MWS::Products::Parsers::Product, @offer_listings.product
+    assert @offer_listings.product.document.size > 0
   end
-
-
 end
