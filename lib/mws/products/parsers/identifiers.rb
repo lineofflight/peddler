c3a84526328cc656963ca658498e742c3230e751
Piotr Misiurek <p.misiurek@gmail.com>
2014-01-23 17:02:53 +0100

    MWS::Products::Parsers::Identifiers

--- lib/mws/products/parsers/identifiers.rb
+++ lib/mws/products/parsers/identifiers.rb
@@ -3,8 +3,23 @@ require 'peddler/parsers/xml'
 module MWS
   module Products
     module Parsers
-      class Identifiers < ::Peddler::Parsers::XML
+      class Identifiers < ::Peddler::Parsers::Model
 
+        value :marketplace_id do
+          text_at_xpath('MarketplaceASIN/MarketplaceId')
+        end
+
+        value :asin do
+          text_at_xpath('MarketplaceASIN/ASIN')
+        end
+
+        value :seller_id do
+          text_at_xpath('SKUIdentifier/SellerId')
+        end
+
+        value :seller_sku do
+          text_at_xpath('SKUIdentifier/SellerSKU')
+        end
       end
     end
   end
