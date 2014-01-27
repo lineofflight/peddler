52e9c3087a693a7bf1be3caa17d95ac4229dcc29
Piotr Misiurek <p.misiurek@gmail.com>
2014-01-27 15:31:47 +0100

    MWS::Products::Parsers::Qualifiers: use symbol_to_xpath

--- lib/mws/products/parsers/qualifiers.rb
+++ lib/mws/products/parsers/qualifiers.rb
@@ -8,7 +8,7 @@ module MWS
         [ :item_condition, :item_subcondition,
           :fulfillment_channel, :seller_positive_feedback_rating].each do |method|
           define_method method do
-            text_at_xpath(method.to_s.split('_').map(&:capitalize).join)
+            text_at_xpath(symbol_to_xpath(method))
           end
         end
 
