1ba38d912d2f1f0029ea155a259ed80e818de5a7
Piotr Misiurek <p.misiurek@gmail.com>
2014-02-03 11:04:05 +0100

    MWS::Feeds: get price feed for integration tests

--- test/integration/feeds/test_feed_submission_result.rb
+++ test/integration/feeds/test_feed_submission_result.rb
@@ -7,11 +7,13 @@ class FeedSubmissionResultTest < IntegrationTest
   def test_gets_feed_submission_result
     VCR.use_cassette('feed_submission_result_test/test_gets_feed_submission_result') do
       @clients.each do |client|
-        feeds = client.get_feed_submission_list(feed_processing_status_list: '_DONE_', max_count: 1)
+        feeds = client.get_feed_submission_list(feed_processing_status_list: '_DONE_',
+          feed_type_list: '_POST_PRODUCT_PRICING_DATA_', max_count: 1)
         feed_submission_id = feeds.first.id
         result = client.get_feed_submission_result(feed_submission_id)
         refute_empty result.to_s
       end
     end
   end
+
 end
