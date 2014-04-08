2c6af4a9417194289e57a12df822d066da97b3d4
hakanensari <hakanensari@gmail.com>
2014-04-08 15:32:16 +0100

    Add stubs for Cart and Customer Information

---
+++ lib/mws/cart_information.rb
@@ -0,0 +1,13 @@
+require 'peddler/client'
+
+module MWS
+  # With the Cart Information API, you can retrieve shopping carts that your
+  # Amazon Webstore customers have created. The Cart Information API enables you
+  # to programmatically integrate Amazon Webstore cart information with your CRM
+  # systems, marketing applications, and other systems that require cart data.
+  #
+  # @todo Not implemented
+  class CartInformation < ::Peddler::Client
+    path 'CartInformation/2014-03-01'
+  end
+end
