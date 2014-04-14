e3a55a9b1a9d09c000633b4b80d9380bbdd4cd5f
hakanensari <hakanensari@gmail.com>
2014-04-14 14:01:06 +0100

    Handle flat-file reports (#11)

--- lib/peddler/parser.rb
+++ lib/peddler/parser.rb
@@ -1,20 +1,24 @@
-require 'peddler/csv_parser'
+require 'peddler/flat_file_parser'
 require 'peddler/xml_parser'
 
 module Peddler
   # @api private
   module Parser
-    def self.parse(res)
-      # Don't parse if there's no body
-      return res unless res.body
+    class << self
+      # The inevitable messiness of massaging data produced by a motley army of
+      # Amazon developers
+      def parse(res, encoding = 'ISO-8859-1')
+        # Don't parse if there's no body
+        return res unless res.body
 
-      case res.headers['Content-Type']
-      when 'text/xml'
-        XMLParser.new(res)
-      when 'application/octet-stream'
-        CSVParser.new(res)
-      else
-        raise NotImplementedError
+        case res.headers['Content-Type']
+        when 'text/xml'
+          XMLParser.new(res)
+        when 'application/octet-stream'
+          FlatFileParser.new(res, encoding)
+        else
+          raise NotImplementedError
+        end
       end
     end
   end
