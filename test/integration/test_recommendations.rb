8b27a44c7219c590653f689a4a06ae10888b1727
hakanensari <hakanensari@gmail.com>
2014-07-11 17:11:12 +0100

    Don't assert if account has no sales

--- test/integration/test_recommendations.rb
+++ test/integration/test_recommendations.rb
@@ -5,14 +5,17 @@ class RecommendationsTest < IntegrationTest
   def test_gets_last_updated_time_for_recommendations
     clients.each do |client|
       res = client.get_last_updated_time_for_recommendations
-      refute_empty res.parse
+      # MWS won't recommend unless account has actual sales
+      skip unless content = res.parse
+      refute_empty content
     end
   end
 
   def test_lists_recommendations
     clients.each do |client|
       res = client.list_recommendations
-      refute_empty res.parse
+      skip unless content = res.parse
+      refute_empty content
     end
   end
 
