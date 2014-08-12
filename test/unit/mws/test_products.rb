043777ee55316fcef94ba58502a3f765c1665ee5
hakanensari <hakanensari@gmail.com>
2014-08-12 12:02:57 +0100

    Refactor clients to same-named files

--- test/unit/mws/test_products.rb
+++ test/unit/mws/test_products.rb
@@ -3,7 +3,7 @@ require 'mws/products'
 
 class MWSProductsTest < MiniTest::Test
   def test_operation_includes_marketplace_id
-    client = MWS::Products.new
+    client = MWS::Products::Client.new
     assert client.operation('Foo').has_key?('MarketplaceId')
   end
 end
