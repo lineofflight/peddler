4bec67d6167a6be6c219e842489d82e5351cd9e5
hakanensari <hakanensari@gmail.com>
2014-08-30 19:38:58 +0100

    Extract marketplace logic into class

--- lib/peddler/client.rb
+++ lib/peddler/client.rb
@@ -1,32 +1,22 @@
+require 'forwardable'
 require 'jeff'
+require 'peddler/marketplace'
 require 'peddler/operation'
 require 'peddler/parser'
 
 module Peddler
   # @abstract Subclass to implement an MWS API section.
   class Client
+    extend Forwardable
     include Jeff
-
-    BadMarketplaceId = Class.new(StandardError)
-
-    HOSTS = {
-      'A2EUQ1WTGCTBG2' => 'mws.amazonservices.ca',
-      'AAHKV2X7AFYLW'  => 'mws.amazonservices.com.cn',
-      'A1PA6795UKMFR9' => 'mws-eu.amazonservices.com',
-      'A1RKKUPIHCS9HS' => 'mws-eu.amazonservices.com',
-      'A13V1IB3VIYZZH' => 'mws-eu.amazonservices.com',
-      'A1F83G8C2ARO7P' => 'mws-eu.amazonservices.com',
-      'A21TJRUUN4KGV'  => 'mws.amazonservices.in',
-      'APJ6JRA9NG5V4'  => 'mws-eu.amazonservices.com',
-      'A1VC38T7YXB528' => 'mws.amazonservices.jp',
-      'ATVPDKIKX0DER'  => 'mws.amazonservices.com'
-    }
 
     attr_accessor :path
     attr_writer :merchant_id, :marketplace_id
     attr_reader :body
 
     alias_method :configure, :tap
+
+    def_delegators :marketplace, :host, :encoding
 
     params('SellerId' => -> { merchant_id })
 
@@ -62,6 +52,10 @@ module Peddler
       @merchant_id ||= ENV['MWS_MERCHANT_ID']
     end
 
+    def marketplace
+      @marketplace ||= find_marketplace
+    end
+
     def defaults
       @defaults ||= { expects: 200 }
     end
@@ -85,33 +79,21 @@ module Peddler
       opts.store(:response_block, blk) if block_given?
       res = post(opts)
 
-      parser.parse(res, host_encoding)
+      parser.parse(res, encoding)
     end
 
     private
+
+    def find_marketplace
+      Marketplace.new(marketplace_id)
+    end
 
     def content_type(str)
       if str.start_with?('<?xml')
         'text/xml'
       else
-        "text/tab-separated-values; charset=#{host_encoding}"
+        "text/tab-separated-values; charset=#{encoding}"
       end
-    end
-
-    def host_encoding
-      if host.end_with?('jp')
-        # Caveat: I've had one instance in the past where Shift_JIS didn't
-        # work but Windows-31J did when parsing a report.
-        'Shift_JIS'
-      elsif host.end_with?('cn')
-        'UTF-16'
-      else
-        'ISO-8859-1'
-      end
-    end
-
-    def host
-      HOSTS.fetch(marketplace_id) { raise BadMarketplaceId }
     end
 
     def extract_options(args)
