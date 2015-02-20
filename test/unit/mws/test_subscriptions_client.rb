cce389fe13dd75f71e52b869b76573e8bacd879f
Hakan Ensari <me@hakanensari.com>
2015-02-20 15:52:36 +0000

    Fix Subscriptions::Client#update_subscription

--- test/unit/mws/test_subscriptions_client.rb
+++ test/unit/mws/test_subscriptions_client.rb
@@ -4,6 +4,41 @@ require 'mws/subscriptions/client'
 class TestSubscriptionsClient < MiniTest::Test
   def setup
     @client = MWS::Subscriptions::Client.new
+
+  def test_updates_subscription
+    operation = {
+      'Action' => 'UpdateSubscription',
+      'MarketplaceId' => '123',
+      'Subscription.IsEnabled' => true,
+      'Subscription.NotificationType' => 'foo',
+      'Subscription.Destination.DeliveryChannel' => 'SQS',
+      'Subscription.Destination.AttributeList.member.1.Key' => 'sqsQueueUrl',
+      'Subscription.Destination.AttributeList.member.1.Value' => 'bar'
+    }
+
+    @client.stub(:run, nil) do
+      @client.update_subscription('foo', 'bar', true)
+    end
+
+    assert_equal operation, @client.operation
+  end
+
+  def test_updates_subscription_with_alternate_marketplace
+    operation = {
+      'Action' => 'UpdateSubscription',
+      'MarketplaceId' => '321',
+      'Subscription.IsEnabled' => true,
+      'Subscription.NotificationType' => 'foo',
+      'Subscription.Destination.DeliveryChannel' => 'SQS',
+      'Subscription.Destination.AttributeList.member.1.Key' => 'sqsQueueUrl',
+      'Subscription.Destination.AttributeList.member.1.Value' => 'bar'
+    }
+
+    @client.stub(:run, nil) do
+      @client.update_subscription('foo', 'bar', true, '321')
+    end
+
+    assert_equal operation, @client.operation
   end
 
   def test_gets_service_status
