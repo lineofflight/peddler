ddbace76d848d5b18650b92e514e78edb4e33deb
Hakan Ensari <me@hakanensari.com>
2015-02-20 00:08:10 +0000

    Fix Reports#update_report_acknowledgements

--- test/unit/mws/test_reports_client.rb
+++ test/unit/mws/test_reports_client.rb
@@ -6,5 +6,19 @@ class TestReportsClient < MiniTest::Test
     @client = MWS::Reports::Client.new
   end
 
+  def test_updates_report_acknowledgements
+    operation = {
+      'Action' => 'UpdateReportAcknowledgements',
+      'Acknowledged' => true,
+      'ReportIdList.Id.1' => '1'
+    }
+
+    @client.stub(:run, nil) do
+      @client.update_report_acknowledgements(true, '1')
+    end
+
+    assert_equal operation, @client.operation
+  end
+
   # FIXME Fill in tests for operations
 end
