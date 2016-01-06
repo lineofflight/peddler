54a421d48bb0cb1571461ea7b7aeeebbc4003438
Hakan Ensari <me@hakanensari.com>
2016-01-06 19:36:49 +0000

    Rewrite VCR matcher

---
+++ test/null_client.rb
@@ -0,0 +1,23 @@
+require 'peddler/client'
+require 'securerandom'
+
+::Peddler::VCRMatcher.ignore_seller!
+
+module Null
+  class Client < ::Peddler::Client
+    def configure_with_mock_data!
+      self.aws_access_key_id = SecureRandom.hex
+      self.aws_secret_access_key = SecureRandom.hex
+      self.merchant_id = SecureRandom.hex
+      self.primary_marketplace_id = 'ATVPDKIKX0DER'
+    end
+  end
+
+  module Parser
+    def self.new(res, *)
+      res
+    end
+  end
+
+  Client.parser = Parser
+end
