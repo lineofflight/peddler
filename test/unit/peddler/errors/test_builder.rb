e28a6d84acdf5521dde1a201d9b803848cbd0f23
Hakan Ensari <hakanensari@gmail.com>
2016-05-23 15:11:57 +0100

    Handle thread safety in error builder

--- test/unit/peddler/errors/test_builder.rb
+++ test/unit/peddler/errors/test_builder.rb
@@ -2,8 +2,15 @@ require 'helper'
 require 'peddler/errors/builder'
 
 class TestPeddlerErrorsBuilder < MiniTest::Test
-  def test_build_error_class
+  def test_builds_error_class
     Peddler::Errors::Builder.build('Foo')
     assert Peddler::Errors::Foo
+  end
+
+  def test_thread_safety
+    Peddler::Errors::Builder.build('Foo')
+    assert_output '', '' do
+      Peddler::Errors::Builder.build('Foo')
+    end
   end
 end
