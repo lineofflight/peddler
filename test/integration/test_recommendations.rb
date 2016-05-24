648c1624a95a63b6af55d2eb3de45b2b869ae16d
Hakan Ensari <hakanensari@gmail.com>
2016-05-24 14:47:47 +0100

    Fix skipped test

--- test/integration/test_recommendations.rb
+++ test/integration/test_recommendations.rb
@@ -5,9 +5,7 @@ class TestRecommendations < IntegrationTest
   def test_lists_recommendations
     clients.each do |client|
       res = client.list_recommendations
-      content = res.parse
-      skip unless content
-      refute_empty content
+      refute_empty res.parse
     end
   end
 
