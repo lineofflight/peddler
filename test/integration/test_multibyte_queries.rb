98a0bac37f7bfe18382037fbce7bc3965d034a49
Hakan Ensari <hakanensari@gmail.com>
2017-01-24 21:28:58 +0000

    Work around Ox not parsing multibyte characters

--- test/integration/test_multibyte_queries.rb
+++ test/integration/test_multibyte_queries.rb
@@ -10,6 +10,7 @@ class TestMultibyteQueries < IntegrationTest
   def test_posts_multibyte_queries_properly
     ret = clients.map do |client|
       res = client.list_matching_products('félix guattari machinic eros')
+      res.body.force_encoding 'UTF-8' if defined? Ox # Ox workaround
       res.parse
     end
     assert ret.map(&:to_s).join.include?('Félix')
