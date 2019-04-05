5fa8a9d97989e81e7ebc0b6b97d15397b5449764
Hakan Ensari <hakanensari@gmail.com>
2019-04-05 13:43:27 +0100

    Add all accessors to test

--- test/integration/test_mws_headers.rb
+++ test/integration/test_mws_headers.rb
@@ -11,6 +11,11 @@ class TestMWSHeaders < IntegrationTest
       res = client.get_lowest_priced_offers_for_asin(client.marketplace.id,
                                                      '1780935374', 'New')
       refute_nil res.mws_quota_max
+      refute_nil res.mws_quota_remaining
+      refute_nil res.mws_quota_resets_on
+      refute_nil res.mws_request_id
+      refute_nil res.mws_timestamp
+      refute_nil res.mws_response_context
     end
   end
 end
