48ab3e9e08b61dbacbb5bd25a67a1e26c6fb4f3e
Hakan Ensari <hakanensari@gmail.com>
2019-04-05 13:53:10 +0100

    Remove test that can't be reproduced with live data

--- test/integration/internals/test_errors.rb
+++ test/integration/internals/test_errors.rb
@@ -14,12 +14,4 @@ class TestErrors < IntegrationTest
       end
     end
   end
-
-  def test_request_throttled
-    clients.each do |client|
-      assert_raises Peddler::Errors::RequestThrottled do
-        client.get_order('foo')
-      end
-    end
-  end
 end
