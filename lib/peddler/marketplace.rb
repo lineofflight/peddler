f1f5ad43ae91781673d812eb799757bb6480e0af
Hakan Ensari <me@hakanensari.com>
2020-01-27 09:59:47 +0000

    Stick to the order in the API docs

--- lib/peddler/marketplace.rb
+++ lib/peddler/marketplace.rb
@@ -52,18 +52,18 @@ module Peddler
     end
 
     @all = [
+      ['A2Q3Y263D00KWC', 'BR', 'mws.amazonservices.com'],
       ['A2EUQ1WTGCTBG2', 'CA', 'mws.amazonservices.com'],
       ['A1AM78C64UM0Y8', 'MX', 'mws.amazonservices.com'],
       ['ATVPDKIKX0DER', 'US', 'mws.amazonservices.com'],
-      ['A2Q3Y263D00KWC', 'BR', 'mws.amazonservices.com'],
       ['A2VIGQ35RCS4UG', 'AE', 'mws.amazonservices.ae'],
       ['A1PA6795UKMFR9', 'DE', 'mws-eu.amazonservices.com'],
       ['A1RKKUPIHCS9HS', 'ES', 'mws-eu.amazonservices.com'],
       ['A13V1IB3VIYZZH', 'FR', 'mws-eu.amazonservices.com'],
-      ['APJ6JRA9NG5V4', 'IT', 'mws-eu.amazonservices.com'],
       ['A1F83G8C2ARO7P', 'GB', 'mws-eu.amazonservices.com'],
-      ['A33AVAJ2PDY3EV', 'TR', 'mws-eu.amazonservices.com'],
       ['A21TJRUUN4KGV', 'IN', 'mws.amazonservices.in'],
+      ['APJ6JRA9NG5V4', 'IT', 'mws-eu.amazonservices.com'],
+      ['A33AVAJ2PDY3EV', 'TR', 'mws-eu.amazonservices.com'],
       ['A39IBJ37TRP1C6', 'AU', 'mws.amazonservices.com.au'],
       ['A1VC38T7YXB528', 'JP', 'mws.amazonservices.jp']
     ].map do |values|
