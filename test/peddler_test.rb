a1114f864d410d4d64cd570aeab23b04b608f324
Hakan Ensari <hakanensari@gmail.com>
2025-05-30 18:17:48 +0200

    chore: add test

--- test/peddler_test.rb
+++ test/peddler_test.rb
@@ -11,5 +11,6 @@ class PeddlerTest < Minitest::Test
   def test_convenience_methods
     assert_instance_of(Peddler::APIs::FBAInventoryV1, Peddler.fba_inventory_v1("us-east-1", "access_token"))
     assert_instance_of(Peddler::APIs::Reports20210630, Peddler.reports_2021_06_30("us-east-1", "access_token"))
+    assert_instance_of(Peddler::APIs::ProductPricing20220501, Peddler.product_pricing("us-east-1", "access_token"))
   end
 end
