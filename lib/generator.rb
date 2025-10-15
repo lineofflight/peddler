6c32c36bb6aea34f46b7c7bc7b2c3f8e553c142e
Hakan Ensari <hakanensari@gmail.com>
2025-10-15 10:01:20 +0200

    chore: fietsshed

--- lib/generator.rb
+++ lib/generator.rb
@@ -14,14 +14,19 @@ module Generator
     def generate
       # Setup introspection loader once before any parallel operations
       # This allows forked processes to inherit the loader without creating new ones
-      IntrospectionLoader.setup
-
+      set_up_introspection_loader
       sync_specs
       generate_apis
       generate_notifications
       generate_reports
       generate_feeds
       generate_entrypoints
+    end
+
+    private
+
+    def set_up_introspection_loader
+      IntrospectionLoader.setup
     end
 
     def sync_specs
