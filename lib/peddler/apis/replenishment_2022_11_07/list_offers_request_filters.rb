c7a04f52a46049f37b5638d0ec3cd6bcaf3986e7
lineoffligbot <4628864+lineoffligbot@users.noreply.github.com>
2026-04-23 03:02:26 +0000

    Update Amazon models

--- lib/peddler/apis/replenishment_2022_11_07/list_offers_request_filters.rb
+++ lib/peddler/apis/replenishment_2022_11_07/list_offers_request_filters.rb
@@ -7,8 +7,8 @@ require "structure"
 module Peddler
   module APIs
     class Replenishment20221107
-      # Use these parameters to filter results. Any result must match all of the provided parameters. For any parameter
-      # that is an array, the result must match at least one element in the provided array.
+      # Use these parameters to filter results. Any result must match all provided parameters. For any parameter that is
+      # an array, the result must match at least one element in the provided array.
       ListOffersRequestFilters = Structure.new do
         # @return [String] The marketplace identifier. The supported marketplaces for both sellers and vendors are US,
         #   CA, ES, UK, FR, IT, IN, DE, and JP. The supported marketplaces for vendors only are BR, AU, MX, AE, and NL.
