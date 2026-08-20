6e754029281c4b766cfd8ae1baaed897b6330bd3
lineoffligbot <4628864+lineoffligbot@users.noreply.github.com>
2026-08-21 01:28:00 +0200

    Update Amazon models (#275)
    Regenerated code from the latest Amazon SP-API models.
    
    Listings Items 2021-08-01: removed the itemized enum-value documentation
    from the categories field on Issue (Amazon trimmed the doc text in the
    spec); the field's type and behavior are unchanged.
    
    No other API, type, or method changes in this update.

--- lib/peddler/apis/listings_items_2021_08_01/issue.rb
+++ lib/peddler/apis/listings_items_2021_08_01/issue.rb
@@ -10,25 +10,6 @@ module Peddler
       # An issue with a listings item.
       Issue = Structure.new do
         # @return [Array<String>] List of issue categories.
-        #
-        # Possible values:
-        #
-        # * 'INVALID_ATTRIBUTE' - Indicating an invalid attribute in the listing.
-        #
-        # * 'MISSING_ATTRIBUTE' - Highlighting a missing attribute in the listing.
-        #
-        # * 'INVALID_IMAGE' - Signifying an invalid image in the listing.
-        #
-        # * 'MISSING_IMAGE' - Noting the absence of an image in the listing.
-        #
-        # * 'INVALID_PRICE' - Pertaining to issues with the listing's price-related attributes.
-        #
-        # * 'MISSING_PRICE' - Pointing out the absence of a price attribute in the listing.
-        #
-        # * 'DUPLICATE' - Identifying listings with potential duplicate problems, such as this ASIN potentially being a
-        #   duplicate of another ASIN.
-        #
-        # * 'QUALIFICATION_REQUIRED' - Indicating that the listing requires qualification-related approval.
         attribute(:categories, [String], null: false)
 
         # @return [String] An issue code that identifies the type of issue.
